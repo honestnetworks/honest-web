2a23fcc0ffc584f2dd974880b25c88953530ed81
Unknown <rostyslav.hutii@orilinc.com>
2018-07-22 01:26:11 +0300

    added back button

---
+++ src/components/BackLink/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { withRouter } from 'react-router-dom';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+
+
+
+const styles = theme => ({
+    linkWrapper: {
+      display: 'flex',
+      color: '#bebfcc',
+      fontSize: '0.8rem',
+      cursor: 'pointer',
+      padding: '1rem 0'
+    },
+    link: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    icon: {
+      paddingRight: '0.5rem'
+    }
+});
+
+const BackLink = (props) => {
+  const {
+    link,
+    backPageTitle,
+    classes,
+    history
+  } = props;
+
+  const handleOnclick = (link) => {
+    return history.push(link);
+  }
+
+  return (
+    <div className="BackLink">
+      <div className={classes.linkWrapper}>
+        <div className={classes.icon}>
+          <ArrowBack />
+        </div>
+        <div 
+          className={classes.link}
+          onClick={() => handleOnclick(link)}
+        >
+          {`Back to the ${backPageTitle}`}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default withStyles(styles)(withRouter(BackLink));
